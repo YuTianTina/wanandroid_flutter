ef2f9d6442be6825fd8f8c59d9e4a7cc8f71c2d5
YuTianTian <xiaotan.yu@tuya.com>
2020-10-21 14:14:59 +0800

    add 插件的开发

--- android/app/src/main/kotlin/com/yutiantina/wanandroid_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/yutiantina/wanandroid_flutter/MainActivity.kt
@@ -1,6 +1,51 @@
 package com.yutiantina.wanandroid_flutter
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    companion object{
+        const val CHANNEL = "samples.flutter.io/battery";
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler(object : MethodChannel.MethodCallHandler{
+            override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                if(call.method == "getBatteryLevel"){
+                    val batteryLevel = getBatteryLevel()
+                    if(-1 != batteryLevel){
+                        result.success(batteryLevel)
+                    }else{
+                        result.error("UNAVAILABLE", "Battery level not available", null)
+                    }
+                }else{
+                    result.notImplemented()
+                }
+            }
+
+        })
+
+    }
+    
+    private fun getBatteryLevel() : Int{
+        var batteryLevel = -1
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        }else{
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) / (intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1))
+        }
+        return batteryLevel;
+    }  
 }
--- lib/ui/homeMe.dart
+++ lib/ui/homeMe.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 /**
  * 首页tab
@@ -12,11 +13,38 @@ class HomeMe extends StatefulWidget{
 }
 
 class _HomeMe extends State<HomeMe>{
+  static const platform =  const MethodChannel('samples.flutter.io/battery');
+  String _batteryLevel = 'Unknown battery level.';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Me'),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Get Battery Level'),
+              onPressed: _getBatteryLevel,
+            ),
+            Text(_batteryLevel)
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<Null> _getBatteryLevel() async{
+    String batteryLevel;
+    try{
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    }on PlatformException catch(e){
+      batteryLevel = "Failed to get battery level: '${e.message}'";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
   }
 
 }
